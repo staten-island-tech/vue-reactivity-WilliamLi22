86e186f22e3baa1c38bc5bc406491aca195e64c2
WilliamLi22 <forvalious@gmail.com>
2023-03-03 10:14:19 -0500

    Redid vue file

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,3 +1,9 @@
-<template></template>
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
 
-<script></script>
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
