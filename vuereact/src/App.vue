1565dd3b197b135e06e3b99abf394a01734c8e89
WilliamLi22 <forvalious@gmail.com>
2023-03-15 10:18:56 -0400

    Finished 1st Video

--- vuereact/src/App.vue
+++ vuereact/src/App.vue
@@ -19,7 +19,6 @@ import HelloWorld from "./components/HelloWorld.vue";
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/about">Profile</RouterLink>
       </nav>
     </div>
   </header>
--- vuereact/src/views/HomeView.vue
+++ vuereact/src/views/HomeView.vue
@@ -7,3 +7,7 @@
 <script setup>
 import TheWelcome from "../components/TheWelcome.vue";
 </script>
+<style scoped></style>
+<!-- scoped makes it so that in this case, the style only applies to this file v-base
+creates template
+ -->
